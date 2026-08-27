bf93fd4f6e99c0b00bfa7adde8567c702ce0660e
Dylan Palmer <djpalmer321@gmail.com>
2026-08-27 17:20:34 -0400

    Update PropertyCard.jsx
    Feat(components): adds PropType to the PropertyCard components

--- frontend/src/components/PropertyCard.jsx
+++ frontend/src/components/PropertyCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import PropertyImageCarousel from "./PropertyImageCarousel";
 import "./PropertyCard.css";
@@ -41,3 +42,17 @@ export default function PropertyCard({ property }) {
         </div>
     );
 }
+
+PropertyCard.propTypes = {
+    property: PropTypes.shape({
+        L_ListingID: PropTypes.string.isRequired,
+        L_Address: PropTypes.string,
+        L_City: PropTypes.string,
+        L_State: PropTypes.string,
+        L_SystemPrice: PropTypes.number,
+        L_Keyword2: PropTypes.number,
+        LM_Dec_3: PropTypes.string,
+        LM_Int2_3: PropTypes.number,
+        L_Photos: PropTypes.string
+    }).isRequired
+};
